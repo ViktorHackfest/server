100eaac2b84e8ec71a30885c60597e4683b9b0e9
risalstr <risa.lestari2002@gmail.com>
2023-02-27 18:26:10 +0700

    fix: change naming method

--- user/models.py
+++ user/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 class UserModel(models.Model):
     id = models.AutoField(primary_key=True)
     displayName = models.CharField(max_length=100)
     email = models.EmailField(max_length=50, unique=True)
-    phoneNumber = models.BigIntegerField()    
+    phoneNumber = models.BigIntegerField()
     photoURL = models.FileField(upload_to="", null=False, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.displayName} - (id= {self.id}) "
